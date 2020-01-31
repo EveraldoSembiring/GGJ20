53424f5944971b5932dfc069c50ddc172f5b8fef
Karunia Ramadhan <rukanishino@gmail.com>
2020-02-01 02:33:32 +0700

    add wwise plugin

--- Source/GGJ20/GGJ20.Build.cs
+++ Source/GGJ20/GGJ20.Build.cs
@@ -8,6 +8,6 @@ public class GGJ20 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AkAudio" });
 	}
 }
