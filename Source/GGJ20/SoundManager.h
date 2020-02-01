5343320d8f9bba6154c3692beebf6a8fb36d26a7
Karunia Ramadhan <rukanishino@gmail.com>
2020-02-01 13:29:53 +0700

    sound manager with wwise callbacks exposed

---
+++ Source/GGJ20/SoundManager.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <AK/SoundEngine/Common/AkTypes.h>
+#include <AKAudio/Classes/AkGameplayTypes.h>
+#include "SoundManager.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSoundDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FUserCueSoundDelegate, FString, userCueName);
+
+UCLASS()
+class GGJ20_API ASoundManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASoundManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	UFUNCTION(BlueprintCallable, Category = "Sounds")
+	void PlayBGM(class UAkAudioEvent* bgmEvent);
+
+	UFUNCTION(BlueprintCallable, Category = "Sounds")
+	void StopBGM(class UAkAudioEvent* bgmEvent);
+
+	UFUNCTION(BlueprintCallable, Category = "Sounds")
+	void PlaySFX(class UAkAudioEvent* sfxEvent);
+
+private:
+	UFUNCTION()
+	void OnBGMCallback(EAkCallbackType CallbackType, UAkCallbackInfo* CallbackInfo);
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Sounds|Events")
+	FSoundDelegate OnBGMStart;
+
+	UPROPERTY(BlueprintAssignable, Category = "Sounds|Events")
+	FSoundDelegate OnBGMEnd;
+
+	UPROPERTY(BlueprintAssignable, Category = "Sounds|Events")
+	FSoundDelegate OnBGMBeat;
+
+	UPROPERTY(BlueprintAssignable, Category = "Sounds|Events")
+	FSoundDelegate OnBGMBar;
+
+	UPROPERTY(BlueprintAssignable, Category = "Sounds|Events")
+	FUserCueSoundDelegate OnBGMCue;
+
+private:
+	int32 BGMid;
+	FOnAkPostEventCallback BGMCallback;
+
+};
